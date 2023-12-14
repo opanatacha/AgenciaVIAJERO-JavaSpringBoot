a2dee88e6e2b91eb22e5957f1d719d6398802b1e
Natacha Carvalho <natachacarvalho90@gmail.com>
2023-12-13 22:23:52 -0300

    Implementação do projeto em Spring Boot
    - Adição das annotations SpringBoot necessárias aos models
    - Criação de interfaces, controllers, services e repositories
    - Implementação de objetos Thymeleaf para tornar páginas HTML dinâmicas
    - Adição de pasta static e configuração WebConfig para armazenar páginas HTML sem controller
    - Adição da pasta Database e modificação do modelo físico do banco de dados

---
+++ VIAJERO_JavaSpringboot/target/classes/static/javascript/alertaexclusaocadastro.js
@@ -0,0 +1,5 @@
+function exibirAlertaExclusao() {
+    if (confirm("Tem certeza que deseja excluir sua conta?")) {
+        alert("Que pena :( sentiremos sua falta. Obrigada por utilizar a Agência VIAJERO!");
+    }
+}
